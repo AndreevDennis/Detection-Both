202f909c676006f8ef41ef49d147ae7941734ddb
Dennis Andreev <denisandreev1611@gmail.com>
2019-11-22 14:55:37 +0200

    w

--- Detection Both/GameViewController.swift
+++ Detection Both/GameViewController.swift
@@ -15,20 +15,40 @@ class GameViewController  : UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false 
         return view
     }()
-    
     let configuration = ARWorldTrackingConfiguration()
     
+    @IBOutlet weak var myButtonPlus: UIButton! = {
+        var imagePlus = UIImage(named: "plus")
+        var button = UIButton(type: .system)
+        button.setImage(imagePlus?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = UIColor(white: 1.0, alpha: 0.7)
+        button.layer.cornerRadius = 40.0
+        button.layer.masksToBounds = true
+        button.layer.zPosition = 1
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }()
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(arView)
+    @IBAction func buttonTapped(_ sender: Any) {
         
+        
+    }
+    
+    
+    fileprivate func constraintsFunc() {
+        view.addSubview(arView)
         arView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         arView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         arView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         arView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    
+    
+    override func viewDidLoad() {
         
+        constraintsFunc()
         
         arView.session.run(configuration, options: [])
         arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
